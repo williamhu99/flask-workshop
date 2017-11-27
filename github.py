4b5b91c43a578ff7a5cd78ad4cedbc3f43b92290
Anushree Agrawal <anushree.agrawal27@gmail.com>
2017-11-26 19:43:47 -0500

    add github numbers

--- github.py
+++ github.py
@@ -1,3 +1,17 @@
 import requests
+import json
 r = requests.get("https://api.github.com/events")
-print r.text
+# print r.text
+# converts to json dictionary
+jsonified = r.json()
+
+print len(jsonified)
+push_event_ct = 0
+other_event_ct = 0
+for entry in jsonified:
+	if entry.get('type') == 'PushEvent':
+		push_event_ct+=1
+	else:
+		other_event_ct+=1 
+print push_event_ct
+print other_event_ct
